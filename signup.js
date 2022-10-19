0e66d082240815e58c3feaa320c6a192b5c457e0
JatinKumarParashar <vkp8015@gmail.com>
2022-10-19 15:05:34 +0530

    Task 2 (Expense Tracker)

--- signup.js
+++ signup.js
@@ -1,6 +1,15 @@
 function signUp(event){
     event.preventDefault();
-    axios.post('http://localhost:4000/user/signup').then((result) => {
+    const user=event.target.name.value;
+    const email=event.target.email.value;
+    const pass=event.target.password.value;
+    const obj={
+        user,
+        email,
+        pass
+    }
+    console.log(obj);
+    axios.post('http://localhost:4000/user/signup',obj).then((result) => {
         console.log(result);
         console.log('routes is working well')
         
